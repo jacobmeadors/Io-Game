54da519aa6916c90219d63cea67f0da49db35280
jacobmeadors <jacob.d.meadors@gmail.com>
2026-03-23 19:58:09 -0500

    Basic setup

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float moveSpeed = 5f;
+
+
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
